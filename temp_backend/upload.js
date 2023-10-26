ae0dce60af4a2acaaa441c677342f574e0606d58
Johan Bjerkem <bjerkem.j@gmail.com>
2023-10-26 14:15:40 +1000

    working upload for python

--- temp_backend/upload.js
+++ temp_backend/upload.js
@@ -1,12 +1,14 @@
 const fetch = require('node-fetch');
 const fs = require('fs');
 
-const presignedUrl = "https://tauro-assignment2.s3.ap-southeast-2.amazonaws.com/1-January.gif?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=ASIA5DYSEEJ46A2XUN4J%2F20231025%2Fap-southeast-2%2Fs3%2Faws4_request&X-Amz-Date=20231025T034934Z&X-Amz-Expires=900&X-Amz-Security-Token=IQoJb3JpZ2luX2VjECkaDmFwLXNvdXRoZWFzdC0yIkcwRQIgKXDe6Wt5ztrRMIYG7DMJrIhdHtmUzFt0z0rdq5i4b8cCIQDJFYIxw67AMBDQccyi0jZfLO6nUpaGpb6duESmlbQcSyqlAwhSEAMaDDkwMTQ0NDI4MDk1MyIM9ZAfLCV5uSrgnVErKoIDweZIi%2BE94HGc18K6IgvCrF9reEuhoA5QTR33EfF5qm7T1sIwUKkWIDzgtSe82WbDgg%2BsaJ%2FF25RPWFQmL0ZOrvPomEUh%2ByGDTGHoywBrvgC9LarOKOhV%2FwSYlzFbJwd1viD7lJQxHE5Kz1sBcV2%2BJHij0md%2FTvdgUM0DMoh%2FlaifW%2BrKPn5xdCsivMIfNe6Dd2eeNxo1qGm1wbPzklKbxpY78QOaOHuS2a0K2PkxPRuT6bZJDObBRAyStBcHDGJm7KcCfDnJCIKW%2Bgd11lrRBa2CIbVzsHIFbB1BjmRiT6KJCz0FzNmc4JSK2kWK6y6TEmN3DSl0t8CY%2BV0PpylIMo%2BlGpajSwF%2F%2Fak4%2FuW6OzFk3QIJUdffYoAhM%2BDoFxK0AAlSqi9bM%2F95chSqfPQZZXkODIxjdyHc2AglSnJrWsUbB5UPE2NB%2FTeHk2azGLucSqNbV8s2jOFIyYL8nBgU%2BJVOJ5fUWUxhiFpkLALQtK4KfAcgvPIJgEQGpr1MEv34e%2FwwkMHhqQY6pgGQ%2FMnJzbm%2Fwric8pkpgpZhC1rHk3pizW9fvA1%2B3CEQnp1qFlTIjFCF9yGi%2Fx97lEH2Mrhjcsjm9CiTyzP5Kf6lTDDEzos9B6FogDuSPWS3P%2F64kLbkAuJhIP0RRBcN9L4AAkj%2FuQMQteWoNvvBu%2F%2Bs4puGdZZjxHfsbBb%2FOTebUhxZlnhHJAi7LKI3wyc4u1FRnJQRZNaNAFXzE2GFbg%2FH3zKwlKjM&X-Amz-Signature=8f3ecc491d071a932b0acb51c33dc08bdacd1431bb47d678fdfa4c2363f40025&X-Amz-SignedHeaders=host";
+const presignedUrl = "https://tauro-assignment2.s3.ap-southeast-2.amazonaws.com/7-November.gif?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=ASIA5DYSEEJ45LZWPVHB%2F20231026%2Fap-southeast-2%2Fs3%2Faws4_request&X-Amz-Date=20231026T040909Z&X-Amz-Expires=900&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEEMaDmFwLXNvdXRoZWFzdC0yIkgwRgIhANkfi%2Fuo3YRwW3pSgycv1xaHIpFOiaR4bfW6GT2PANazAiEAgq%2BOtQXKbdOJR%2F9C66SruH4JJ3CazM86qQW8jYsRXLYqpQMIbBADGgw5MDE0NDQyODA5NTMiDCEZ3Fie0W3%2FqJTbNCqCA%2FPPB%2BcPAj2CNL%2Fd02pAs51sg%2Bot4F9ANGN%2Bh3g89ISqIGDfl9AnEXhACGyY5KyF1xcFlKTqGXT5lyUL3wLKmbZ%2B2kxditLrCXHqrMHIUY4luib6I1euvUrM%2BPSOl3wGEpz7e2zfjAHVOB%2F3rJjFXcN869iEfvZRpvbcUbgGqEKNiu6w26IGVoU%2FLo0DEv%2BmYtjeQ3nkgf7WBX%2Bw2ylDihk8cvnEyVrrpp7w%2F2G9pEwsbm4Dfc8b9DKoeDJ5bQSTVTTPeEG94mlYfBH80HZF4Lbl532p%2FmW8sHR9HUv883tfxUBn5o6AslaMDUo6lkmsI7pIyGxUBmAujRuOMupTBf1oXQz2lAMe1LqNC4LLk9yhDTPyIoRq2YiW8KsO7WePKxpKmVulygnfsvTJco0MR6NtRDczqWAiz6TfVT%2BbCekVW0BsWY1fdq3BpWCImutlTI7ZxToyT2UBVFHOB2dWKx7UKMfbK6%2F5nN%2B9dqMeB%2FzIAGUTLWo3aWMiS8RqfFfPq26RMNKc56kGOqUB25DleGMcFSbRs09Ts8KdavV1y9SZPm%2BmukowkztsJd3BDPLrEtsBJMznvjp8TunlnArLcdEtF89tPkEq235M7DE6nQx6fxhWWo0lau6gLWvjyZy16S8p4PSuBfVecO3DOZJnoZNQ8Z2o%2Fz8txZvEOGGVenKD7Uudo%2FTr69xkNMzM9TenXRJneH1DNt9%2BIp%2Fgavnw9tNAxa8ls6nosf4%2FbF8DlE3n&X-Amz-Signature=238891322a5285cf16113e7e4577ded474496e682972f457791716290716b0d9&X-Amz-SignedHeaders=host";
 
 async function uploadToS3() {
     const fileBuffer = fs.readFileSync('earth.gif');
     
     try {
+        console.time('Upload Time');  // Start the timer with a label
+
         const response = await fetch(presignedUrl, {
             method: 'PUT',
             body: fileBuffer,
@@ -14,6 +16,8 @@ async function uploadToS3() {
                 'Content-Type': 'image/gif'
             }
         });
+
+        console.timeEnd('Upload Time');  // End the timer with the same label
 
         if (response.status === 200) {
             console.log('Successfully uploaded to S3');
